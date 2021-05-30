249612c29821059fe540fe09f42bb848e8a2618a
Francis Cruz <jeni13franc@gmail.com>
2021-05-30 17:37:14 +0530

    Updates

---
+++ precache-manifest.29545ab3023dd47e24bcb1543dcf0895.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7474f6847aef16c019737a3e8ff5f1a6",
+    "url": "/javascript-calculator-react/index.html"
+  },
+  {
+    "revision": "86039f56fdab0f133da0",
+    "url": "/javascript-calculator-react/static/css/main.588582fe.chunk.css"
+  },
+  {
+    "revision": "79b164f4817be59c8389",
+    "url": "/javascript-calculator-react/static/js/2.c88e5bfb.chunk.js"
+  },
+  {
+    "revision": "86039f56fdab0f133da0",
+    "url": "/javascript-calculator-react/static/js/main.50519321.chunk.js"
+  },
+  {
+    "revision": "e29f283abf3be93a66d0",
+    "url": "/javascript-calculator-react/static/js/runtime~main.3b4978b0.js"
+  }
+]);
